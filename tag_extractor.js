3c88e73a72a4f2fbab9ee457a566e1871b685373
YashBoss <yashksri199747@gmail.com>
2020-04-05 19:09:13 +0530

    first commit

--- tag_extractor.js
+++ tag_extractor.js
@@ -20,6 +20,50 @@ async function UserExtractor(tag) {
   return await userData;
 }
 
+const getTagData = async tag => {
+  var r = [];
+  await axios
+    .get("https://www.instagram.com/explore/tags/" + tag + "/?__a=1", {
+      headers: {
+        "user-agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+      }
+    })
+    .then(async response => {
+      // handle success
+      var res = response.data.graphql.hashtag.edge_hashtag_to_media.edges;
+      r = res;
+    })
+    .catch(e => console.log(e));
+  return await r;
+};
+
+const getUserNames = async edge => {
+  var r = "i";
+  var owner_id = edge.node.owner.id;
+  var url = "https://i.instagram.com/api/v1/users/" + owner_id + "/info/";
+
+  await axios
+    .get(url, {
+      headers: {
+        "user-agent":
+          "Mozilla/5.0 (Linux; U; Android 5; nl-nl; GT-P6800 Build/HTJ85B) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 123.1.0.26.115 (iPhone11,8; iOS 13_3; en_US; en-US; scale=2.00; 828x1792; 190542906"
+      }
+    })
+    .then(response => {
+      // handle success
+      var res = response.data.user.username;
+      r = res;
+    })
+    .catch(e => {
+      console.log(e.response.status);
+      r = null;
+    });
+  return await r;
+};
+
+exports.UserExtractor = UserExtractor;
+
 // var c = 0;
 //     const n = res.length;
 //     res.forEach(edge => {
@@ -89,47 +133,3 @@ async function UserExtractor(tag) {
 //     // handle error
 //     console.log("qq");
 //   });
-
-const getTagData = async tag => {
-  var r = [];
-  await axios
-    .get("https://www.instagram.com/explore/tags/" + tag + "/?__a=1", {
-      headers: {
-        "user-agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
-      }
-    })
-    .then(async response => {
-      // handle success
-      var res = response.data.graphql.hashtag.edge_hashtag_to_media.edges;
-      r = res;
-    })
-    .catch(e => console.log(e));
-  return await r;
-};
-
-const getUserNames = async edge => {
-  var r = "i";
-  var owner_id = edge.node.owner.id;
-  var url = "https://i.instagram.com/api/v1/users/" + owner_id + "/info/";
-
-  await axios
-    .get(url, {
-      headers: {
-        "user-agent":
-          "Mozilla/5.0 (Linux; U; Android 5; nl-nl; GT-P6800 Build/HTJ85B) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 123.1.0.26.115 (iPhone11,8; iOS 13_3; en_US; en-US; scale=2.00; 828x1792; 190542906"
-      }
-    })
-    .then(response => {
-      // handle success
-      var res = response.data.user.username;
-      r = res;
-    })
-    .catch(e => {
-      console.log(e.response.status);
-      r = null;
-    });
-  return await r;
-};
-
-exports.UserExtractor = UserExtractor;
